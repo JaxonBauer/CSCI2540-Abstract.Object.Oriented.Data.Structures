0d308fdce2e59f6b02458369a74a53e6910930b8
Jaxon Bauer <103966964+JaxonBauer@users.noreply.github.com>
2022-09-15 15:57:26 -0400

    Add files via upload

---
+++ Account.Project/CheckingAccount.java
@@ -0,0 +1,153 @@
+/**
+ * Jaxon Bauer
+ * 
+ * Assignment 3 - Checking Account Class
+ * 
+ * Subclass of Account parent class
+ */
+
+package assg3_bauerj21;
+
+public class CheckingAccount extends Account
+{
+	/**
+	 * Instance Variables
+	 */
+	
+	private double overdraft;
+	static double fee = 20.0;
+	
+	/**
+	 * Single Parameter Constructor referring to parent class Account
+	 */
+	
+	public CheckingAccount(String acctNo)
+	{
+		super(acctNo);
+		overdraft = 0;
+		this.setBalance(0);
+	}
+	
+	/**
+	 * Three Parameter Constructor referring to parent class Account
+	 */
+	
+	public CheckingAccount(String acctNo, double balance, double overdraft)
+	{
+		super(acctNo);
+		this.setBalance(balance);
+		this.setOverdraft(overdraft);
+	}
+	
+	/**
+	 * Get / Set methods used to call and store values for overdraft and fee.
+	 */
+	
+	public Double getOverdraft()
+	{
+		return this.overdraft;
+	}
+	
+	public void setOverdraft(double overdraft)
+	{
+		this.overdraft = overdraft;
+	}
+	
+	public double getFee()
+	{
+		return CheckingAccount.fee;
+	}
+	
+	public void setFee(double fee)
+	{
+		CheckingAccount.fee = fee;
+	}
+	
+	/**
+	 * toString method used to convert given information into a string and return it to the screen.
+	 */
+	
+	public String toString()
+	{
+		return displayInfo();
+	}
+	
+	/**
+	 * Withdraw method used to take a given amount of money out of one's account.
+	 * Does not change the original value of the object.
+	 * Bounds are set to avoid withdrawing negative money, or more money than is in the account.
+	 * Messages will be printed to tell the user what is happening throughout the transaction.
+	 */
+	
+	public void withdraw(double amount)
+	{
+		if (amount < 0)
+		{
+			System.out.println("ERROR: Negative number detected. Cannot withdraw negative amount.\n");
+		}
+		else if(amount > this.getBalance() + this.getOverdraft())
+		{
+			System.out.println("ERROR: Desired amount exceeds the overdraft limit.");
+			System.out.println("Transaction cannot be completed.\n");
+		}
+		else if (amount > this.getBalance() && amount < this.getBalance() + this.getOverdraft())
+		{
+			System.out.println(displayInfo());
+			System.out.println(String.format("Transaction will be charged the overdraft fee of $%.2f\n", this.getFee()));
+			System.out.println(String.format("Amount withdrawn: $%.2f\n", amount));
+			System.out.println(String.format("New Balance: $%.2f\n", (this.getBalance() - amount - getFee())));
+		}
+		else if (amount < this.getBalance())
+		{
+			System.out.println(displayInfo());
+			System.out.println(String.format("Amount withdrawn: $%.2f\n", amount));
+			System.out.println(String.format("New Balance: $%.2f\n", this.getBalance() - amount));
+		}
+	}
+	
+	/**
+	 * Transfer method used to move money from one account to another while not changing original values.
+	 * Bounds are set to where we cannot transfer more money than the overdraft limit allows.
+	 * Bounds are also set to where we cannot withdraw negative money.
+	 * We can have a negative value for our balance, but only if it stays under the allowed overdraft limit.
+	 * The overdraft fee for each account is $20.00.
+	 */
+	
+	public void transfer(Object account, double amount)
+	{
+		if(amount > this.getBalance() + this.getOverdraft())
+		{
+			System.out.println("ERROR: Desired amount exceeds the overdraft limit.");
+			System.out.println("Transaction cannot be completed.\n");
+		}
+		else if(amount > this.getBalance() && amount < this.getBalance() + this.getOverdraft())
+		{
+			System.out.println(String.format("Amount before transfer (Account #%s): $%.2f", this.getAcctNo(), this.getBalance())); 
+			System.out.println(String.format("Amount before transfer (Account #%s): $%.2f\n" + "\nAmount transferred: $%.2f\n", ((Account) account).getAcctNo(), ((Account) account).getBalance(),  amount));
+			System.out.println(String.format("Transaction will be charged the overdraft fee of $%s\n", this.getFee()));
+			System.out.println(String.format("Amount after transfer (Account #%s): $%.2f", this.getAcctNo(), (this.getBalance() - amount - fee)));
+			System.out.println(String.format("Amount after transfer (Account #%s): $%.2f\n", ((Account) account).getAcctNo(), (((Account) account).getBalance() + amount)));
+		}
+		else if (amount > 0 && amount < this.getBalance())
+		{
+			System.out.println(String.format("Amount before transfer (Account #%s): $%.2f", this.getAcctNo(), this.getBalance())); 
+			System.out.println(String.format("Amount before transfer (Account #%s): $%.2f\n" + "\nAmount transferred: $%.2f\n", ((Account) account).getAcctNo(), ((Account) account).getBalance(),  amount));
+			System.out.println(String.format("Amount after transfer (Account #%s): $%.2f", this.getAcctNo(), (this.getBalance() - amount)));
+			System.out.println(String.format("Amount after transfer (Account #%s): $%.2f\n", ((Account) account).getAcctNo(), (((Account) account).getBalance() + amount)));
+		}
+		else
+		{
+			System.out.println("ERROR: Negative number detected. Cannot transfer negative amount.\n");
+		}
+	}
+	
+	/**
+	 * displayInfo method used to format the information regarding a given account.
+	 */
+	
+	public String displayInfo()
+	{
+		return String.format("Account number: %s" + "\n" + "Current balance: $%.2f" + "\n" + "Overdraft limit: $%.2f" + "\n", this.getAcctNo(), this.getBalance(), this.getOverdraft());
+	}
+	
+}
